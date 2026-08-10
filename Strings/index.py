a5011a9e75586e09721c044f07d1aa33ffa260b2
Mohd Mehran Siddiqui <mmehran0703@yahoo.in>
2026-08-10 14:20:52 +0530

    feat(Strings in python)

---
+++ Strings/index.py
@@ -0,0 +1,21 @@
+# Single string
+string = "Hi this is a string."
+# String with apostrophe
+
+apostrphe = "This is python's string"
+
+# String with double quote
+
+doublequote = 'We are learning "Python"'
+
+# Unformatted string
+first = "Mehran"
+last = "Siddiqui"
+name = first + " [ " + last + " ]"
+print(name)
+
+# For formatted string the string is prefixed by the alphabet f
+
+formatted_name = f"{first} [ {last} ] is my name"
+print(formatted_name)
+# The formatted string is much cleaner
