085cc3024886a0788524cda566648c38030c9e7c
Mohd Mehran Siddiqui <mmehran0703@yahoo.in>
2026-08-10 14:11:32 +0530

    feat(Variables in Python)

---
+++ Variables/Variable.py
@@ -0,0 +1,10 @@
+patient_name = "Mehran"
+
+# The variables are defined as above there is no neet to define the type is it a variable, constant or Let type. It simply is a VARIABLE
+
+
+# Printiting a VARIABLE
+
+# We use predefined method print()
+
+print(patient_name)
