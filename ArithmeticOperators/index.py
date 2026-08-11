6e7cc5248cf8302478218944949fc61ce87e691f
Mohd Mehran Siddiqui <mmehran0703@yahoo.in>
2026-08-11 15:01:12 +0530

    feat(Arithmetic operators)

---
+++ ArithmeticOperators/index.py
@@ -0,0 +1,20 @@
+# Just like any other programming language python also has
+# arithmetic operators
+# +-*/
+# Additionally python has two more operators ** and //
+
+import math
+primary_number = 10
+add = primary_number+2
+sub = primary_number-2
+multiply = primary_number*2
+divide = primary_number/2
+
+
+roundoff_divide = primary_number//3
+power = primary_number**2
+
+# for  math functions we can import the math module using the below given command
+
+
+print(math.sqrt(primary_number))
