d23a56086533deb2db0ecfad0f3fa7977d7e63d4
Mohd Mehran Siddiqui <mmehran0703@yahoo.in>
2026-08-11 15:21:33 +0530

    feat(Conditions in Python)

---
+++ ConditionalStatement/index.py
@@ -0,0 +1,14 @@
+marks_obtained = input("Please enter the marks obtained: ")
+
+numberMarks = float(marks_obtained)
+
+if (numberMarks >= 90):
+    print("The student has got A grade")
+elif (numberMarks >= 75 and numberMarks < 90):
+    print("The student has got B grade")
+elif (numberMarks >= 60 and numberMarks < 75):
+    print("The student has got C grade")
+elif (numberMarks >= 40 and numberMarks < 60):
+    print("The student has got D grade")
+else:
+    print("Unfortunately! The student has failed")
